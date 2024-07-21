e81421258c9fcc59d9612611d69c5dafabca829b
ogamache <o.gamache2@gmail.com>
2024-07-21 15:17:43 -0400

    Adapt mono code to save the maps and reset everytime it breaks

--- Main/mono/run_borealhdr_mono.sh
+++ Main/mono/run_borealhdr_mono.sh
@@ -5,4 +5,4 @@ exposure_time=4.0
 experiment=forest_04-21-2023
 file_name=backpack_2023-04-21-08-51-27
 
-./Main/mono/mono_borealhdr Vocabulary/ORBvoc.txt Main/mono/calib_files/BOREALHDR_april.yaml ../data/$experiment/data_high_resolution/$file_name/camera_left/$exposure_time/
+./Main/mono/mono_borealhdr Vocabulary/ORBvoc.txt Main/mono/calib_files/BOREALHDR_april.yaml ../../data/$experiment/data_high_resolution/$file_name/camera_left/$exposure_time/ ../results/$file_name/orbslam2/mono/$exposure_time/
